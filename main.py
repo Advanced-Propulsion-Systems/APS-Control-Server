487b6d71155e039f5aae1945b4f58e476f0bf3c9
Jpuc1143 <jaime.perez@uc.cl>
2023-10-27 21:47:39 -0300

    Feat: implement recording

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import asyncio
 import random
+import os
+import uuid
+from io import TextIOWrapper
 
 
 class ControlServer:
     def __init__(self) -> None:
         self.read_count = 0
         self.websockets: set[WebSocket] = set()
+
+        self.recording_file: TextIOWrapper | None = None
+        self.recording_count = 0
 
     def sensor_metadata(self):
         sensors = []
@@ -36,17 +42,52 @@ class ControlServer:
             else:
                 raise NotImplementedError("Real data readings not implemented yet")
 
+            if self.recording_file is not None:
+                self.recording_file.write(str(self.recording_count))
+                for key, value in data.items():
+                    self.recording_file.write("," + str(value[1]))
+                self.recording_file.write("\n")
+                self.recording_count += 1
+
             msg = {"type": "data", "data": data}
             for websocket in self.websockets:
                 await websocket.send_json(msg)
 
             self.read_count += 1
 
+    async def broadcast_msg(self, msg: dict):
+        async with asyncio.TaskGroup() as tg:
+            for websocket in self.websockets:
+                tg.create_task(websocket.send_json(msg))
+
+    async def update_status(self, **kwargs):
+        await self.broadcast_msg({"type": "update-status", "changes": kwargs})
+
     async def receive_msg(self, socket: WebSocket):
         try:
             while True:
                 msg = await socket.receive_json()
-                print(msg)
+                if msg["cmd"] == "start-recording":
+                    if self.recording_file is not None:
+                        continue
+
+                    id = uuid.uuid4().hex
+                    self.recording_file = open(
+                        os.path.join("recordings", id + ".csv"), "w", buffering=1
+                    )
+                    self.recording_count = 0
+                    await self.update_status(recordingStatus=True)
+
+                elif msg["cmd"] == "stop-recording":
+                    if self.recording_file is None:
+                        continue
+
+                    self.recording_file.close()
+                    self.recording_file = None
+                    await self.update_status(recordingStatus=False)
+
+                elif msg["cmd"] == "ignite":
+                    raise NotImplementedError
 
         except WebSocketDisconnect:
             self.websockets.remove(socket)
@@ -63,6 +104,7 @@ manager = ControlServer()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    os.makedirs("recordings", exist_ok=True)
     broadcast_task = asyncio.create_task(manager.broadcast_msgs())
     yield
     broadcast_task.cancel()
